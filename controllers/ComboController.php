f4f532876677cef3ed3dc6ce094c94e671c49089
Valery-Arroyo <vlae.barrantes@gmail.com>
2026-06-16 08:23:01 -0600

    Correción en ComboController y documentación añadida

--- controllers/ComboController.php
+++ controllers/ComboController.php
@@ -1,12 +1,23 @@
 <?php
 class Combo
 {
+    // Método para obtener todos los combos
     public function index()
     {
         try {
+            //La clase Response en tu backend es una utilidad que se encarga de 
+           // formatear y devolver las respuestas al cliente (frontend) de manera consistente.
             $response = new Response();
+
+            //Crea una instancia del modelo ComboModel, 
+            //que se encarga de interactuar con la base de datos
             $combo = new ComboModel();
+
+            //Llama al método "all" del modelo para obtener todos los combos 
+            // disponibles en la base de datos
             $result = $combo->all();
+
+            //Utiliza el método "toJSON" del objeto Response para formatear el resultado
             $response->toJSON($result);
         } catch (Exception $e) {
             handleException($e);
@@ -16,9 +27,18 @@ class Combo
     public function get($categoria)
     {
         try {
+            // Crea un objeto Response, encargado de formatear la respuesta en JSON
             $response = new Response();
-            $producto = new ComboModel();
-            $result = $producto->get($categoria);
+
+            // Crea una instancia del modelo ComboModel, que se encarga de 
+            // interactuar con la base de datos
+            $combo = new ComboModel();
+
+            // Llama al método "get" del modelo, pasando la categoría como parámetro,
+            // para obtener los combos que pertenecen a esa categoría
+            $result = $combo->get($categoria);
+
+            // Utiliza el método "toJSON" del objeto Response para formatear el resultado
             $response->toJSON($result);
         } catch (Exception $e) {
             handleException($e);
@@ -28,9 +48,17 @@ class Combo
     public function getCombo($id)
     {
         try {
+
+            // Crea un objeto Response, encargado de formatear la respuesta en JSON
             $response = new Response();
-            $producto = new ProductoModel();
-            $result = $producto->get($id);
+
+            // Crea una instancia del modelo ComboModel, que se encarga de 
+            // interactuar con la base de datos
+            $combo = new ComboModel();
+
+            // Llama al método "get" del modelo, pasando el ID como parámetro,
+            // para obtener el combo que corresponde a ese ID
+            $result = $combo->get($id);
             $response->toJSON($result);
         } catch (Exception $e) {
             handleException($e);
--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -1,8 +1,11 @@
 <?php
 class Producto
 {
+    // Método para obtener todos los productos
     public function index()
     {
+        // La clase Response en tu backend es una utilidad que se encarga de 
+        // formatear y devolver las respuestas al cliente (frontend) de manera consistente.
         try {
             $response = new Response();
             $producto = new ProductoModel();
@@ -13,24 +16,44 @@ class Producto
         }
     }
 
+    // Método para obtener productos por categoría
     public function get($categoria)
     {
         try {
+            // Crea un objeto Response, encargado de formatear la respuesta en JSON
             $response = new Response();
+
+            // Crea una instancia del modelo ProductoModel, que se encarga de 
+            //interactuar con la base de datos
             $producto = new ProductoModel();
+
+            // Llama al método "get" del modelo, pasando la categoría como parámetro,
+            // para obtener los productos que pertenecen a esa categoría
             $result = $producto->get($categoria);
+
+            // Utiliza el método "toJSON" del objeto Response para formatear el resultado
             $response->toJSON($result);
         } catch (Exception $e) {
             handleException($e);
         }
     }
 
+    // Método para obtener un producto por su ID
     public function getProducto($id)
     {
         try {
+            // Crea un objeto Response, encargado de formatear la respuesta en JSON
             $response = new Response();
+
+            // Crea una instancia del modelo ProductoModel, que se encarga de 
+            //interactuar con la base de datos
             $producto = new ProductoModel();
+
+            // Llama al método "get" del modelo, pasando el ID como parámetro,
+            // para obtener el producto que corresponde a ese ID
             $result = $producto->get($id);
+
+            // Utiliza el método "toJSON" del objeto Response para formatear el resultado
             $response->toJSON($result);
         } catch (Exception $e) {
             handleException($e);
